3d4b6e9f60aeb59ac89348da9e5ac7d7ca4590a9
Sophia Lorena <batatafrita03177@gmail.com>
2025-10-09 20:52:38 -0300

    modificando o service G5

--- src/main/java/br/com/hospital/service/ConsultaService.java
+++ src/main/java/br/com/hospital/service/ConsultaService.java
@@ -2,4 +2,38 @@ package br.com.hospital.service;
 
 public class ConsultaService {
 
+import br.com.hospital.model.Consulta;
+import br.com.hospital.repository.ConsultaRepository;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ConsultaService {
+    private final ConsultaRepository repo = new ConsultaRepository();
+
+    //nova consulta
+    public void inserir(Consulta obj) throws SQLException {
+        repo.inserir(obj);
+    }
+
+    //lista consulta
+    public List<Consulta> listarTodos() throws SQLException {
+        return repo.listarTodos();
+    }
+
+    //buscar consulta
+    public Consulta buscarPorId(int id) throws SQLException {
+        return repo.buscarPorId(id);
+    }
+
+    //atualizar dados
+    public void atualizar(Consulta obj) throws SQLException {
+        repo.atualizar(obj);
+    }
+
+    //excluir consulta
+    public void excluir(int id) throws SQLException {
+        repo.excluir(id);
+    }
+}
+
 }
--- src/main/java/br/com/hospital/service/PacienteService.java
+++ src/main/java/br/com/hospital/service/PacienteService.java
@@ -1,5 +1,38 @@
 package br.com.hospital.service;
 
 public class PacienteService {
+    import br.com.hospital.model.Paciente;
+import br.com.hospital.repository.PacienteRepository;
+import java.sql.SQLException;
+import java.util.List;
+
+public class PacienteService {
+    private final PacienteRepository repo = new PacienteRepository();
+
+    //inserir paciente
+    public void inserir(Paciente obj) throws SQLException {
+        repo.inserir(obj);
+    }
+
+    //listar paciente
+    public List<Paciente> listarTodos() throws SQLException {
+        return repo.listarTodos();
+    }
+
+    //buscar paciente por ID
+    public Paciente buscarPorId(int id) throws SQLException {
+        return repo.buscarPorId(id);
+    }
+
+    //atualizar paciente existente
+    public void atualizar(Paciente obj) throws SQLException {
+        repo.atualizar(obj);
+    }
+
+    //excluir paciente pelo ID
+    public void excluir(int id) throws SQLException {
+        repo.excluir(id);
+    }
+}
 
 }
