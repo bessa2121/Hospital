8f61288e5a1b4f0ca4bfef678a5cbae8054b544d
Felipe Rocha <felipergomes19@gmail.com>
2025-10-09 08:36:54 -0300

    Cria os metodos construtores das tabelas 'Receita', 'ReceitaRemedio' e 'Remedio'

--- src/main/java/br/com/hospital/model/Receita.java
+++ src/main/java/br/com/hospital/model/Receita.java
@@ -1,5 +1,59 @@
 package br.com.hospital.model;
 
 public class Receita {
+    private int id_receita;
+    private int id_paciente;
+    private int id_funcionario;
+    private date data_emissao;
+    private string observacoes;
 
+    public Receita(){}
+    public Receita(int id_receita,int id_paciente, int id_funcionario, date data_emissao, string observacoes){
+        this.id_receita = id_receita;
+        this.id_paciente = id_paciente;
+        this.id_funcionario = id_funcionario;
+        this.data_emissao = data_emissao;
+        this.observacoes = observacoes;
+    }
+
+    //Getters e Setters
+    public int getIdReceita(int id_receita){
+        return id_receita;
+    }
+    public void setIdReceita(int id_receita){
+        this.id_receita = id_receita;
+        }
+    
+    public int getIdPaciente(int id_paciente){
+        return id_paciente;
+    }
+    public void setIdPaciente(int id_paciente){
+        this.id_paciente = id_paciente;
+    }
+
+    public int getIdFuncionario(id_funcionario){
+        return id_funcionario;
+    }
+    public void setIdFuncionario(int id_funcionario){
+        this.id_funcionario = id_funcionario;
+    }
+
+    public date getDataEmissao(date data_emissao){
+        return data_emissao;
+    }
+    public void setDataEmissao(date data_emissao){
+        this.data_emissao = data_emissao;
+    }
+
+    public string getObservacoes(string observacoes){
+        return observacoes;
+    }
+    public void setObservacoes(string observacoes){
+        this.observacoes = observacoes;
+    }
+
+    @Override
+    public String toString(){
+    return "Receita [id_receita = "+ id_receita+", id_paciente = "+id_paciente+", id_funcionario = "+ id_funcionario+", data = "+data_emissao+" Observações = "+observacoes"         ]"
+    }
 }
--- src/main/java/br/com/hospital/model/Remedio.java
+++ src/main/java/br/com/hospital/model/Remedio.java
@@ -1,5 +1,36 @@
 package br.com.hospital.model;
 
 public class Remedio {
+    private int id_remedio;
+    private string nome;
+    private string descricao;
 
+    public Remedio(){}
+    public Remedio(int id_remedio, string nome, string descricao){
+        this.id_remedio = remedio;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    //GETTER E SETTERS
+    public int getIdRemedio(int id_remedio){
+        return remedio;
+    }
+    public void setIdRemedeio(int id_remedio){
+        this.id_remedio = id_remedio;
+    }
+
+    public string getNome(string nome){
+        return nome;
+    }
+    public void setNome(string nome){
+        this.nome = nome;
+    }
+
+    public string getDescricao(string descricao){
+        return descricao;
+    }
+    public void setDescricao(string descricao){
+        this.descricao = descricao;
+    }
 }
