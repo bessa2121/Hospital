6b898cf3e8847d8baefebcb51ac3865688fd0697
Julia Akemi <schwtsz13@gmail.com>
2025-10-16 08:28:02 -0300

    Atualização model/Sala

--- src/main/java/br/com/hospital/model/Sala.java
+++ src/main/java/br/com/hospital/model/Sala.java
@@ -1,5 +1,24 @@
 package br.com.hospital.model;
 
 public class Sala {
+    private int idSala;
+    private int idFuncionario;
+}
 
+public Sala() {}
+
+public Sala(int idSala, int idFuncionario) {
+    this.idSala = idSala;
+    this.idFuncionario = idFuncionario;
+}
+
+public int getidSala() { return idSala; } 
+public void setidSala(int idSala) { this.idSala = idSala; }
+
+public int getidFuncionario() { return idFuncionario; } 
+public void setidFuncionario(int idSala) { this.idSala = idFuncionario; }
+
+@Override 
+public String toString() {
+    return "id Sala = " + idSala;
 }
